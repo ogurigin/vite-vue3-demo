8cf888ef20d2d3536deea54b94e8eb989c2fc971
oguri_Xx <315980986@qq.com>
2021-05-12 16:42:46 +0800

    init

---
+++ src/utils/axios.ts
@@ -0,0 +1,22 @@
+import Axios from 'axios'
+
+const baseURL = '..'
+
+const axios = Axios.create({
+    baseURL,
+    timeout: 20000
+})
+
+// 前置拦截器（发起请求之前的拦截）
+axios.interceptors.request.use(
+    (response) => {
+        /**
+         * 根据你的项目实际情况来对 config 做处理
+         * 这里对 config 不做任何处理，直接返回
+         */
+        return response
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
